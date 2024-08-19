c8022fc14265b17a525c7bec517f567d2d364b77
vinc3leong <vinceleong04@gmail.com>
2024-08-19 15:09:29 +0800

    changed chatbot name

---
+++ src/main/java/Skibidi.java
@@ -0,0 +1,8 @@
+public class Skibidi {
+    public static void main(String[] args) {
+        String name = "Skibidi";
+        System.out.println("Ohio skibidi on the wall, who's the skibidiest of 'em all?!! " +
+                "\nHello! I'm " + name + " the skibidiest of 'em all"+ " \nWhat can I do for you?");
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
